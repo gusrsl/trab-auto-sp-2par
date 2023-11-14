49f3f44f54db61e030935febb9734097baa0bfd0
Gustavo Rodriguez <89959859+gusrsl@users.noreply.github.com>
2023-11-14 16:52:48 -0500

    prueba

--- apps/ws-server/src/medico/dto/create-medico.dto.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable prettier/prettier */
-export class CreateMedicoDto {
-    readonly usuario: string;
-    readonly password: string;
-    readonly nombre: string;
-    readonly especialidad: string;
-    readonly mail: string;
-    readonly celular: string;
-}
